bf21264c65f1579ae210d3b3ca7e3d0e394f88ab
hubluesky <hubluesky@126.com>
2025-06-18 11:05:33 +0800

    1、资源整理

--- Assets/Plugins/FFmpeg/Helpers/FFmpegBinariesHelper.cs
+++ Assets/Plugins/FFmpeg/Helpers/FFmpegBinariesHelper.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public static class FFmpegBinariesHelper
 {
-    public static void RegisterFFmpegBinaries()
-    {
-        string path = GetFFmpegBinaryPath();
-        Debug.Log($"FFmpeg Path: {path}");
-        ffmpeg.RootPath = path;
-        
-    }
+        public static void RegisterFFmpegBinaries()
+        {
+                string path = GetFFmpegBinaryPath();
+                Debug.Log($"FFmpeg Path: {path}");
+                ffmpeg.RootPath = path;
+        }
 
-    private static string GetFFmpegBinaryPath()
-    {
+        private static string GetFFmpegBinaryPath()
+        {
 #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
-        return Path.Combine(Application.dataPath, "Plugins", "FFmpeg", "Windows");
+                return Path.Combine(Application.dataPath, "Plugins", "FFmpeg", "Windows");
 #elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
-        return Path.Combine(Application.dataPath, "Plugins", "FFmpeg", "MacOS");
+                var arch = System.Runtime.InteropServices.RuntimeInformation.OSArchitecture;
+                if (arch == System.Runtime.InteropServices.Architecture.Arm64)
+                    return Path.Combine(Application.dataPath, "Plugins", "FFmpeg", "MacOS_Arm64");
+                else
+                    return Path.Combine(Application.dataPath, "Plugins", "FFmpeg", "MacOS");
 #elif UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX
-        return Path.Combine(Application.dataPath, "Plugins", "FFmpeg", "Linux");
+                return Path.Combine(Application.dataPath, "Plugins", "FFmpeg", "Linux");
 #else
-        throw new NotSupportedException("Platform not supported.");
+                throw new NotSupportedException("Platform not supported.");
 #endif
-    }
+        }
 }
